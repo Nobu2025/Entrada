72935fcab945747bdcbb663b9f1e90ad82e35945
Nobu2025 <20243rd038@utez.edu.mx>
2025-05-09 09:31:14 -0600

    se pidio el nombre y el apellido

---
+++ Entrada.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+public class Entrada{
+    public static void main (String[] args){
+            String nombreUsuario = JOptionPane.showInputDialog("ingresa el nombre");
+            System.out.println("Tu nombre es: "+ nombreUsuario);
+            
+            
+    }
+}
